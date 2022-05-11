8f9f20dde776c864c54a8862751569fd05ebfc58
Doops <vickyd178>
2022-05-11 16:15:46 +0530

    Episode 3 :  Images

--- app/src/main/java/com/avion/composedemo/MainActivity.kt
+++ app/src/main/java/com/avion/composedemo/MainActivity.kt
@@ -3,43 +3,106 @@ package com.avion.composedemo
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
-import androidx.compose.foundation.border
-import androidx.compose.foundation.clickable
-import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.fillMaxHeight
-import androidx.compose.foundation.layout.fillMaxWidth
-import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material.Card
 import androidx.compose.material.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Brush
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.painter.Painter
+import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.text.TextStyle
 import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
-            Column(
-                modifier = Modifier
-                    .background(Color.Cyan)
-                    .fillMaxWidth()
-                    .fillMaxHeight(0.5f)
-                    .border(10.dp, color = Color.Red)
-                    .padding(10.dp)
-                    .border(10.dp, color = Color.Green)
-                    .padding(10.dp)
-                    .border(10.dp, color = Color.Yellow)
-                    .padding(20.dp)
+            Row(modifier = Modifier.fillMaxWidth()) {
+                Box(
+                    modifier = Modifier
+                        .weight(1.0f)
+                        .fillMaxWidth()
+                        .padding(16.dp)
+                ) {
+                    ImageCard(
+                        painter = painterResource(id = R.drawable.ironman),
+                        description = "Iron man aka Tony Stark",
+                        title = "Iron man aka Tony Stark"
+                    )
+                }
+                Box(
+                    modifier = Modifier
+                        .weight(1.0f)
+                        .fillMaxWidth()
+                        .padding(16.dp)
+                ) {
 
-            ) {
-                Text(text = "Hello World!")
-                Text(text = "What's up?", modifier = Modifier
-                    .border(5.dp,color = Color.Blue)
-                    .padding(10.dp)
-                    .clickable {  }
-
-                )
+                    ImageCard(
+                        painter = painterResource(id = R.drawable.ironman),
+                        description = "Iron man aka Tony Stark",
+                        title = "Iron man aka Tony Stark"
+                    )
+                }
             }
+        }
+    }
+}
+
+@Composable
+fun ImageCard(
+    painter: Painter,
+    description: String,
+    title: String,
+    modifier: Modifier = Modifier
+) {
+
+    Card(
+        modifier = modifier
+            .fillMaxWidth(),
+        shape = RoundedCornerShape(15.dp),
+        elevation = 5.dp
+    ) {
+        Box(modifier = Modifier.height(200.dp)) {
+            Image(
+                painter = painter,
+                contentDescription = description,
+                contentScale = ContentScale.Crop
+            )
+
+            Box(
+                modifier = Modifier
+                    .fillMaxSize()
+                    .background(
+                        brush = Brush.verticalGradient(
+                            colors = listOf(
+                                Color.Transparent,
+                                Color.Black
+                            ),
+                            startY = 300f
+                        )
+                    )
+            ) {
+
+            }
+
+            Box(
+                modifier = Modifier
+                    .fillMaxSize()
+                    .padding(12.dp),
+                contentAlignment = Alignment.BottomStart
+            ) {
+                Text(text = title, style = TextStyle(color = Color.White, fontSize = 16.sp))
+            }
+
+
         }
     }
 
